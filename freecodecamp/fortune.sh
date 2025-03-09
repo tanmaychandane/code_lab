59dbb5bf7ad7391cd9d7148b9991c6deb8990ea4
Tanmay Narendra Chandane <33379183+tanmaychandane@users.noreply.github.com>
2025-03-09 13:50:26 +0530

    final

--- freecodecamp/fortune.sh
+++ freecodecamp/fortune.sh
@@ -2,11 +2,21 @@
 #Program to tell a persons fortune
 echo -e "\n"~~ Fortune Teller ~~"\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-#echo ${RESPONSES[5]}
 N=$(( RANDOM % 6 ))
-#echo ${RESPONSES[$N]}
+
 GET_FORTUNE() {
-echo Ask a yes or no question:
+  if [[ ! $1 ]]
+  then
+  echo Ask a yes or no question:
+  else
+  echo Try again. Make sure it ends with a question mark:
+  fi
+  read QUESTION
 }
 GET_FORTUNE
-echo ${RESPONSES[$N]}
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE again
+done
+
+echo -e "\n${RESPONSES[$N]}"
